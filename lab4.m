b212cf61e6d9ae32d0e881919f06bf7877251a40
Semin Bae <s6bae@uwaterloo.ca>
2018-03-08 13:19:29 -0500

    Lab 4 - Prelab-preparatiobn

--- lab4.m
+++ lab4.m
@@ -3,7 +3,17 @@ I_s = 651.9;
 H_s = 651.9;
 Ps = 17.59/(s^2+2.2*s-42.31);
 
+
 Kp = 0.37034;
+Kp2 =3.91*(10-3);
+Cs_P1 = Kp;
+Cs_P2 = Kp2;
+
+Tcl_P2 = (I_s*Cs_P2*Ps)/(1+H_s*Cs_P2*Ps);
+period_P2 = 1/(bandwidth(Tcl_P2)/(2*pi))/10;
+Tcl_P1 = (I_s*Cs_P1*Ps)/(1+H_s*Cs_P1*Ps);
+period_P1 = 1/(bandwidth(Tcl_P1)/(2*pi))/10;
+
 % rlocus(Ps);
 % PD
 % calculating  Kd:
@@ -14,8 +24,13 @@ T_PD1 = s/(s^2+2.20*s+4204);
 Kd_PD1 = 46.7/4204;
 Cs_PD1 = Kp+Kd_PD1*s;
 Tcl_PD1 = (I_s*Cs_PD1*Ps)/(1+H_s*Cs_PD1*Ps);
-bandwidth_PD1 = bandwidth(Tcl_PD1)/(2*pi);
 period_PD1 = 1/(bandwidth(Tcl_PD1)/(2*pi))/10;
+
+Kd_PD2 = 45/4204;
+Cs_PD2 = Kp+Kd_PD2*s;
+Tcl_PD2 = (I_s*Cs_PD2*Ps)/(1+H_s*Cs_PD2*Ps);
+period_PD2 = 1/(bandwidth(Tcl_PD2)/(2*pi))/10;
+
 % period_PD1
 
 
@@ -26,8 +41,26 @@ T_PID1 = s^2/(s^3+2.2*s^2+4204*s+11467);
 % Tcl_PID1 = (I_s*Cs_PD1*Ps)/(1+H_s*Cs_PD1*Ps);
 
 T_PID2 = 1/(s^3+116.8692*s^2+4204*s);
-rlocus(T_PID2);
+% rlocus(T_PID2);
 Cs_PID1 = 0.37034 + 0.0305/s + 0.0100*s;
 Tcl_PD1 = (I_s*Cs_PID1*Ps)/(1+H_s*Cs_PID1*Ps);
 
 period_PID2 = 1/(bandwidth(Tcl_PD1)/(2*pi))/10;
+
+
+T_PID3 = 1/(s^3+116.8692*s^2+4204*s);
+Cs_PID3 = 0.37034 + 0.0043/s + 0.0157*s;
+Tcl_PID3 = (I_s*Cs_PID3*Ps)/(1+H_s*Cs_PID3*Ps);
+period_PID3 = 1/(bandwidth(Tcl_PID3)/(2*pi))/10;
+
+rlocus(T_PD1);
+hold on;
+plot(-23, 23.4647, 'kx', 'Markersize', 10);
+plot(-23, -23.4647, 'kx', 'Markersize', 10);
+x = -400:0.01:0
+y1 = (23.4647/-23)*x;
+y2 = (-23.4647/-23)*x;
+plot(x, y1,'k');
+plot(x, y2, 'k');
+hold off;
+period_PD2
